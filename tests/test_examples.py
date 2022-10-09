b4eed2ea822f90b53acff0d4d892f3a348beb524
ashitabh <ashitabh.x.kumar@gsk.com>
2022-10-09 10:09:00 +0530

    .

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,2 +1,17 @@
-def test_sample():
+# examples of unit test cases
+
+def sum(x, y):
+    return x+y
+
+def test_sum():
+    x = 5
+    y = 7
+    z = sum(x,y)
+    expected_z = 12
+    assert z == expected_z
+
+def test_equal():
     assert 1==1
+
+
+
