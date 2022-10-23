b2a53752eb5278a1ddf2a813e034545d38a74949
ashitabh <ashitabh.x.kumar@gsk.com>
2022-10-23 09:44:29 +0530

    .

---
+++ plot_digits_classification-decisiontree.py
@@ -0,0 +1,127 @@
+"""
+================================
+Recognizing hand-written digits
+================================
+
+This example shows how scikit-learn can be used to recognize images of
+hand-written digits, from 0-9.
+
+"""
+
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+
+# Standard scientific Python imports
+import matplotlib.pyplot as plt
+
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics,tree
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from skimage.transform import resize
+from tabulate import tabulate
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+###############################################################################
+# Digits dataset
+# --------------
+#
+# The digits dataset consists of 8x8
+# pixel images of digits. The ``images`` attribute of the dataset stores
+# 8x8 arrays of grayscale values for each image. We will use these arrays to
+# visualize the first 4 images. The ``target`` attribute of the dataset stores
+# the digit each image represents and this is included in the title of the 4
+# plots below.
+#
+# Note: if we were working from image files (e.g., 'png' files), we would load
+# them using :func:`matplotlib.pyplot.imread`.
+
+
+def process(train_split,dev_split):
+    digits = datasets.load_digits()
+
+
+        ###############################################################################
+        # Classification
+        # --------------
+        #
+        # To apply a classifier on this data, we need to flatten the images, turning
+        # each 2-D array of grayscale values from shape ``(8, 8)`` into shape
+        # ``(64,)``. Subsequently, the entire dataset will be of shape
+        # ``(n_samples, n_features)``, where ``n_samples`` is the number of images and
+        # ``n_features`` is the total number of pixels in each image.
+        #
+        # We can then split the data into train and test subsets and fit a support
+        # vector classifier on the train samples. The fitted classifier can
+        # subsequently be used to predict the value of the digit for the samples
+        # in the test subset.
+
+        #############################################################################
+        # Resize of image
+
+
+    # flatten the images
+    n_samples = len(digits.images)
+    # print Size of original image
+    print(f"Image shape resized : {digits.images[0].shape}")
+    data = digits.images.reshape((n_samples, -1))
+    data = pd.DataFrame(StandardScaler().fit_transform(data))
+
+        # Split data into  train and test
+    split = 1-train_split
+    X_train, X_dev, y_train, y_dev = train_test_split(
+            data, digits.target, test_size=split, shuffle=True, random_state=1
+        )
+    X_test, X_dev, y_test, y_dev = train_test_split(
+            X_dev, y_dev, test_size=(1 - train_split), shuffle=True, random_state=1
+        )
+
+    print(X_train.shape), print(y_train.shape)
+    print(X_dev.shape), print(y_dev.shape)
+    print(X_test.shape), print(y_test.shape)
+
+        # Create a classifier: a support vector classifier
+    clf = tree.DecisionTreeClassifier()
+
+        #hyper_params = best_hyperparam['hyperparams']
+        #clf.set_params(**hyper_params)
+
+        # Learn the digits on the train subset
+    clf.fit(X_train, y_train)
+
+        # Predict the value of the digit on the test subset
+    predicted = clf.predict(X_test)
+
+    print(
+            f"Classification report for classifier {clf}:\n"
+            f"{metrics.classification_report(y_test, predicted)}\n"
+        )
+
+        ###############################################################################
+        # We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
+        # true digit values and the predicted digit values.
+
+    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+    disp.figure_.suptitle("Confusion Matrix")
+    print(f"Confusion matrix:\n{disp.confusion_matrix}")
+
+    plt.show()
+
+def main():
+    # Split size ( 0.8 to 0.7)
+    split_list=[0.75,0.80,0.85]
+    for g in split_list:
+        train_split = g
+        dev_split = (1-g)
+        
+
+        process(train_split,dev_split)
+    
+
+if __name__ == "__main__":
+    main()
